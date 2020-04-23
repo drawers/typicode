1ae559acc85b6f6ebdeea12dd9775c5f0e73d74f
David Rawson <david.rawson@trademe.co.nz>
2020-04-24 10:23:23 +1200

    Initial commit

---
+++ app/src/main/java/nz/co/typicode/MainActivity.kt
@@ -0,0 +1,18 @@
+package nz.co.typicode
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import nz.co.typicode.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/nz/co/typicode/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package nz.co.typicode.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
