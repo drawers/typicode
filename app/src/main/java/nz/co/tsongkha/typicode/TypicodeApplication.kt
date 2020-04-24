68d8d3b6385ee01fa01851bd900156be5c57a08b
David Rawson <david.rawson@trademe.co.nz>
2020-04-24 13:43:45 +1200

    Set up modules

--- app/src/main/java/nz/co/tsongkha/typicode/TypicodeApplication.kt
+++ app/src/main/java/nz/co/tsongkha/typicode/TypicodeApplication.kt
@@ -1,6 +1,7 @@
 package nz.co.tsongkha.typicode
 
 import android.app.Application
+import nz.co.tsongkha.typicode.network.networkModule
 import toothpick.ktp.KTP
 
 class TypicodeApplication : Application() {
@@ -8,6 +9,6 @@ class TypicodeApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         KTP.openScope(ApplicationScope::class.java)
-            .installModules()
+            .installModules(networkModule)
     }
 }
