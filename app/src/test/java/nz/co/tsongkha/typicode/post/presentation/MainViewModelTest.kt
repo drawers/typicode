3ab6bcd9a3b0438e531ebbc78766b44085aa8539
David Rawson <david.rawson@trademe.co.nz>
2020-04-26 10:50:53 +1200

    Hook up mock web server

--- app/src/test/java/nz/co/tsongkha/typicode/post/presentation/MainViewModelTest.kt
+++ app/src/test/java/nz/co/tsongkha/typicode/post/presentation/MainViewModelTest.kt
@@ -1,10 +1,13 @@
 package nz.co.tsongkha.typicode.post.presentation
 
+import androidx.lifecycle.Observer
 import io.kotest.core.spec.style.StringSpec
+import io.mockk.mockk
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.TestCoroutineDispatcher
 import nz.co.tsongkha.typicode.network.testNetworkModule
 import nz.co.tsongkha.typicode.post.data.postsDataModule
+import nz.co.tsongkha.typicode.post.presentation.bento.PostItem
 import toothpick.ktp.KTP
 
 @ExperimentalCoroutinesApi
@@ -25,9 +28,18 @@ class MainViewModelTest : StringSpec() {
         val viewModel = MainViewModel()
         scope.inject(viewModel)
 
+        val itemsObserver = mockk<Observer<List<PostItem>>>(relaxed = true)
+
+        println("adding observer")
+
         "it should" {
+            viewModel.posts.observeForever(itemsObserver)
             println("goodbye")
             viewModel.load()
+            viewModel.posts.removeObserver(itemsObserver)
         }
+
+        println("removing observer")
+
     }
 }
--- network/src/main/java/nz/co/tsongkha/typicode/network/client/MockHttpClientProvider.kt
+++ network/src/main/java/nz/co/tsongkha/typicode/network/client/MockHttpClientProvider.kt
@@ -3,11 +3,12 @@ package nz.co.tsongkha.typicode.network.client
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.mock.MockEngine
 import io.ktor.client.engine.mock.respond
+import io.ktor.client.features.json.GsonSerializer
+import io.ktor.client.features.json.JsonFeature
 import io.ktor.http.ContentType
 import io.ktor.http.HttpStatusCode
 import io.ktor.http.fullPath
 import io.ktor.http.headersOf
-import nz.co.tsongkha.typicode.network.service.BASE_URL
 import toothpick.InjectConstructor
 import java.io.File
 import javax.inject.Provider
@@ -17,27 +18,40 @@ class MockHttpClientProvider : Provider<HttpClient> {
 
     override fun get(): HttpClient {
         return HttpClient(MockEngine) {
+
+            install(JsonFeature) {
+                serializer = GsonSerializer()
+            }
+
             engine {
                 addHandler { request ->
                     when {
-                        request.url.fullPath == "$BASE_URL/posts" -> {
-                            val body = File("nz/co/tsongkha/typicode/network/client/posts.json").readBytes()
+                        request.url.fullPath == "/posts" -> {
+                            val body = File("../network/src/main/java/nz/co/tsongkha/typicode/network/client/posts.json").readBytes()
                             respond(
                                 content = body,
                                 status = HttpStatusCode.OK,
-                                headers = headersOf("Content-Type" to listOf(ContentType.Application.Json.toString()))
+                                headers = headersOf(
+                                    "Content-Type" to listOf(
+                                        ContentType.Application.Json.toString()
+                                    )
+                                )
                             )
                         }
-                        request.url.encodedPath.matches(
+                        request.url.fullPath.matches(
                             Regex(
                                 "/posts/.+/comments"
                             )
                         ) -> {
-                            val body = File("nz/co/tsongkha/typicode/network/client/comments.json").readBytes()
+                            val body = File("..network/src/main/java/nz/co/tsongkha/typicode/network/client/comments.json").readBytes()
                             respond(
                                 content = body,
                                 status = HttpStatusCode.OK,
-                                headers = headersOf("Content-Type" to listOf(ContentType.Application.Json.toString()))
+                                headers = headersOf(
+                                    "Content-Type" to listOf(
+                                        ContentType.Application.Json.toString()
+                                    )
+                                )
                             )
                         }
                         else -> {
@@ -46,6 +60,7 @@ class MockHttpClientProvider : Provider<HttpClient> {
                     }
                 }
             }
+
         }
     }
 }
