9cf3706cf4919fd4d3989864082ca82484746a41
David Rawson <david.rawson@trademe.co.nz>
2020-04-27 11:40:28 +1200

    Fix up build script slightly

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,34 +39,37 @@ dependencies {
 
     implementation(project(":network"))
 
-    implementation("com.yelp.android:bento:15.8.0")
-    androidTestImplementation("com.yelp.android:bento-testing:15.8.0")
+    val bentoVersion = "15.8.0"
+    implementation("com.yelp.android:bento:$bentoVersion")
+    androidTestImplementation("com.yelp.android:bento-testing:$bentoVersion")
 
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.5")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.3.5")
+    val coroutinesVersion = "1.3.5"
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:$coroutinesVersion")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:$coroutinesVersion")
 
     implementation("androidx.appcompat:appcompat:1.1.0")
+    implementation("androidx.cardview:cardview:1.0.0")
     implementation("androidx.core:core-ktx:1.2.0")
     implementation("androidx.constraintlayout:constraintlayout:1.1.3")
-    implementation("androidx.cardview:cardview:1.0.0")
     implementation("com.google.android.material:material:1.1.0")
-    implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.2.0")
     implementation("com.facebook.shimmer:shimmer:0.5.0")
 
-    implementation("com.github.stephanenicolas.toothpick:ktp:3.1.0")
-    implementation("com.github.stephanenicolas.toothpick:smoothie-lifecycle-viewmodel-ktp:3.1.0")
-    implementation("com.github.stephanenicolas.toothpick:smoothie-lifecycle-ktp:3.1.0")
-    kapt("com.github.stephanenicolas.toothpick:toothpick-compiler:3.1.0")
+    val lifecycleVersion = "2.2.0"
+    implementation("androidx.lifecycle:lifecycle-extensions:$lifecycleVersion")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycleVersion")
 
+    val toothpickVersion = "3.1.0"
+    implementation("com.github.stephanenicolas.toothpick:ktp:$toothpickVersion")
+    implementation("com.github.stephanenicolas.toothpick:smoothie-lifecycle-viewmodel-ktp:$toothpickVersion")
+    implementation("com.github.stephanenicolas.toothpick:smoothie-lifecycle-ktp:$toothpickVersion")
+    kapt("com.github.stephanenicolas.toothpick:toothpick-compiler:$toothpickVersion")
 
-    testImplementation(project(":network"))
-    testImplementation("junit:junit:4.13")
-    testImplementation("io.kotest:kotest-runner-junit5-jvm:4.0.4")
-    testImplementation("io.kotest:kotest-assertions-core-jvm:4.0.4")
-    testImplementation("io.kotest:kotest-property-jvm:4.0.4")
+    val kotestVersion = "4.0.4"
+    testImplementation("io.kotest:kotest-runner-junit5-jvm:$kotestVersion")
+    testImplementation("io.kotest:kotest-assertions-core-jvm:$kotestVersion")
+    testImplementation("io.kotest:kotest-property-jvm:$kotestVersion")
 
-    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.3.5")
     testImplementation("io.mockk:mockk:1.10.0")
 
     androidTestImplementation("androidx.test.ext:junit:1.1.1")
